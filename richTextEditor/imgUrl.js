778812249aef67c9aa02dd58d4f5463ec63a385e
stark920 <hautang920@gmail.com>
2022-04-01 01:20:49 +0800

    update richTextEditor

--- richTextEditor/imgUrl.js
+++ richTextEditor/imgUrl.js
@@ -1,5 +1,3 @@
-Quill.register('modules/imageUploader', ImageUploader);
-
 const uploadImgPath =
   'https://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80';
 
@@ -17,14 +15,8 @@ const editor = new Quill('#description', {
   modules: {
     toolbar: {
       container: toolbarOptions,
-    },
-    imageUploader: {
-      upload: (file) => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve(uploadImgPath);
-          }, 1000);
-        });
+      handlers: {
+        image: imageHandler,
       },
     },
     imageResize: {
@@ -32,3 +24,11 @@ const editor = new Quill('#description', {
     },
   },
 });
+
+function imageHandler() {
+  var range = this.quill.getSelection();
+  var value = prompt('please copy paste the image url here.');
+  if (value) {
+    this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
+  }
+}
